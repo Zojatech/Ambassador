b044f240c37003c3ff919e2b59a08ae7d72f9574
drew-chidi <chidi.andrew@gmail.com>
2023-04-05 19:31:03 +0100

    added font

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -8,11 +8,11 @@ const Hero = () => {
         {/* Title and Intro */}
         <div className='flex flex-col relative flex-1 mb-8 lg:mb-0'>
           <div></div>
-          <h1 className='text-3xl lg:text-[2.5rem] font-bold mb-6'>
+          <h1 className='text-3xl lg:text-[2.5rem] font-bold mb-4 lg:mb-6'>
             Become an <br />
             aspiring ambassador
           </h1>
-          <p className=' lg:text-xl text-gray-text mb-14 w-5/6'>
+          <p className=' lg:text-xl text-gray-text mb-5 lg:mb-14 lg:w-5/6'>
             Join our community and represent our brand as an ambassador. you’ll
             have the opportunity to represent zojapay and make meaningful
             connections with others who share your passion for positive Change.{' '}
