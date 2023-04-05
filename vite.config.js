4ade358adb1d410924a3a79dcc1570bc06d4018d
drew-chidi <chidi.andrew@gmail.com>
2023-04-05 17:58:57 +0100

    removed unused dependency

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,12 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       '@': '/src', // This is the same as the "baseUrl" in jsconfig.json
-//     },
-//   },
-// });
-
-// vite.config.js
-import reactRefresh from '@vitejs/plugin-react-refresh';
-
-export default {
-  plugins: [reactRefresh()],
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       '@': '/src', // This is the same as the "baseUrl" in jsconfig.json
     },
   },
-};
+});
