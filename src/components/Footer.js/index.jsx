08837de880fd21db025b8383101233904b8fd12c
drew-chidi <chidi.andrew@gmail.com>
2023-04-06 14:10:46 +0100

    resolve bug with deployment

--- src/components/Footer.js/index.jsx
+++ src/components/Footer.js/index.jsx
@@ -1,34 +1,30 @@
-import andriod from "../../assets/googlestore.svg";
-import ios from "../../assets/appstore.svg";
-import brand from "../../assets/zojapay.png";
-import iphone1 from "../../assets/iphone-front.png";
-import iphone3 from "../../assets/iphone-right.svg";
-import iphone2 from "../../assets/iphoneleft.png";
+import andriod from '../../assets/googlestore.svg';
+import ios from '../../assets/appstore.svg';
+import brand from '../../assets/zojapay.png';
+import Phone from '../../assets/phone-group.png';
 
 const Footer = () => {
   return (
     <footer>
-      <div className="image">
-        <div className="download">
+      <div className='image'>
+        <div className='download'>
           <h3>download zojapay now!</h3>
-          <div className="btn">
-            <a href="">
+          <div className='btn'>
+            <a href=''>
               <img src={andriod}></img>
             </a>
-            <a href="">
+            <a href=''>
               <img src={ios}></img>
             </a>
           </div>
         </div>
-        <div className="iphones">
-          <img src={iphone2} className="i2" />
-          <img src={iphone1} className="i1" />
-          <img src={iphone3} className="i3" />
+        <div className='iphones'>
+          <img src={Phone} className='i2' />
         </div>
       </div>
 
-      <div className="text">
-        <div className="left">
+      <div className='text'>
+        <div className='left'>
           <img src={brand} />
           <p>
             zoja is a peer-2-peer financial inclusion pplication that simplifies
@@ -36,8 +32,8 @@ const Footer = () => {
           </p>
         </div>
 
-        <div className="lists">
-          <div className="product">
+        <div className='lists'>
+          <div className='product'>
             <h3>Product feature</h3>
             <ul>
               <li>Cash request</li>
@@ -46,7 +42,7 @@ const Footer = () => {
             </ul>
           </div>
 
-          <div className="product">
+          <div className='product'>
             <h3>comapny</h3>
             <ul>
               <li>Terms and condition</li>
@@ -55,7 +51,7 @@ const Footer = () => {
             </ul>
           </div>
 
-          <div className="product">
+          <div className='product'>
             <h3>Support</h3>
             <ul>
               <li>Gbagada lagos</li>
@@ -63,13 +59,13 @@ const Footer = () => {
               <li>
                 follows us
                 <a href>
-                  <i className="fa-brands fa-facebook-f"></i>
+                  <i className='fa-brands fa-facebook-f'></i>
                 </a>
                 <a href>
-                  <i className="fa-brands fa-twitter"></i>
+                  <i className='fa-brands fa-twitter'></i>
                 </a>
                 <a href>
-                  <i className="fa-brands fa-instagram"></i>
+                  <i className='fa-brands fa-instagram'></i>
                 </a>
               </li>
             </ul>
