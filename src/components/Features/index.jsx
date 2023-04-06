d256225ebabfa8282980f9cc3911ba05f7253b52
iamdonemmy <abiodunemma0394@gmail.com>
2023-04-06 19:53:10 +0100

    create ambassdor section

--- src/components/Features/index.jsx
+++ src/components/Features/index.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import Testimonial from '@/components/Testimonial';
-import Develop from '@/components/Develop';
-import Benefits from '@/components/Benefits';
+import React from "react";
+import Testimonial from "@/components/Testimonial";
+import Develop from "@/components/Develop";
+import Benefits from "@/components/Benefits";
+import Ambass from "@/components/ambass";
 
 const Features = () => {
   return (
-    <div className='px-6 pt-12 lg:px-28 lg:pt-28 lg:pb-20'>
+    <div className="px-6 pt-12 lg:px-28 lg:pt-28 lg:pb-20">
       <Develop />
       <Testimonial />
 
       {/* WHAT IS IN FOR YOU */}
       <Benefits />
+
+      <Ambass />
     </div>
   );
 };
---
+++ src/components/ambass/index.jsx
@@ -0,0 +1,34 @@
+import ambass from "../../assets/ambass.jpg";
+
+const Ambass = () => {
+  return (
+    <section className="ambass">
+      <h2>All you need to become an Ambassador</h2>
+
+      <div className="details">
+        <div className="image">
+          <img src={ambass} />
+        </div>
+
+        <div className="need">
+          <h3>
+            To become a Zojapay Ambassador, you need to be bold and creative.
+            also these are needed:
+          </h3>
+          <ul>
+            <li>Be passionate about financial education</li>
+            <li>Be eager to learn effective commnication skills</li>
+            <li>
+              Be excited about creating innovation and showcasing creativity
+            </li>
+            <li>Be enthusiastic about teamwork and collaboration</li>
+          </ul>
+
+          <button type="button">Become an Ambassador</button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Ambass;
