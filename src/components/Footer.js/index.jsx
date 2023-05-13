816db9e08d19ba49f178fb7d07bd107b9f0c5cfd
drew-chidi <chidi.andrew@gmail.com>
2023-05-13 16:39:35 +0100

    minor fixes on footer

--- src/components/Footer.js/index.jsx
+++ src/components/Footer.js/index.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
         <div className='absolute bottom-0 right-0 -z-10'>
           <img src={Waves} alt='wavy background' />
         </div>
-        <div className='px-4 py-[10px] text-sm md:mt-3 md:flex md:items-center  md:justify-between '>
+        <div className='px-4 py-[10px] text-sm md:mt-3 md:flex md:items-center  md:justify-between md:gap-14 lg:gap-28'>
           <div className='md:w-[30%]'>
             <div className='w-[90px] md:w-full'>
               <img src={Brand} alt='Zojapay brand logo' />
@@ -21,9 +21,16 @@ const Footer = () => {
               Zojapay is a peer-2-peer financial inclusion application that
               simplifies payment by making access to cash easy or retail market
             </p>
+            <div className='hidden text-xs text-primary md:mt-3 md:block'>
+              <a>
+                <span className='text-primary'> &#169; 2020</span> Zojapay. All
+                Right Reserved
+              </a>
+            </div>
           </div>
 
-          <div className=' md:flex md:w-3/5 md:justify-between'>
+          {/* QUICK LINKS */}
+          <div className=' md:flex md:w-[70%] md:justify-between md:gap-4'>
             <div className='mx-0 mb-4'>
               <h3 className='font-semibold capitalize'>Product Features</h3>
               <ul className='mt-2.5 flex flex-col gap-2.5'>
@@ -79,7 +86,7 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className='text-black-500 dark:hover:text-blue ml-3.5 text-xs hover:text-blue-900 '>
+        <div className='text-black-500 dark:hover:text-blue ml-3.5 text-xs hover:text-blue-900 md:hidden '>
           <a>
             <span className=''> &#169; 2020</span> Zojapay.All Right Reserved
           </a>
