3d932a90681bfd1cac86de03d9087d24a0e4dd6b
drew-chidi <chidi.andrew@gmail.com>
2023-05-25 16:10:28 +0100

    facebook pixel implementation

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,20 @@ import Hero from '@/components/Hero';
 import Features from '@/components/Features';
 import Footer from '@/components/Footer.js';
 import Faqs from '@/components/Faqs';
+import ReactPixel from 'react-facebook-pixel';
+import dotenv from 'dotenv';
+
 
 function App() {
+// Load the environment variables
+dotenv.config();
+
+// Initialize the Facebook Pixel
+ReactPixel.init(process.env.REACT_APP_PIXEL_ID);
+
+// Track a page view
+ReactPixel.pageView();
+
   return (
     <div>
       <Navbar />
